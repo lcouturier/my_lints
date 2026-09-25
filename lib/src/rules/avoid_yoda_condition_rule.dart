e5f355c5fadd76448b468aa1d59df54e339f23c5
Laurent Couturier <laurent_couturier@connect-tech.sncf>
2026-09-25 11:53:47 +0200

    refactor(avoid_yoda_conditions): :recycle: utiliser le pattern matching pour améliorer la lisibilité des conditions
    * Remplace la logique de vérification des expressions binaires par un pattern matching.
    * Simplifie le code en rendant la condition plus explicite.

--- lib/src/rules/avoid_yoda_condition_rule.dart
+++ lib/src/rules/avoid_yoda_condition_rule.dart
@@ -2,6 +2,7 @@ import 'package:analyzer/analysis_rule/analysis_rule.dart';
 import 'package:analyzer/analysis_rule/rule_context.dart';
 import 'package:analyzer/analysis_rule/rule_visitor_registry.dart';
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/error/error.dart';
 import 'package:my_lints/src/common/extensions.dart';
@@ -75,11 +76,16 @@ class _ConditionVisitor extends RecursiveAstVisitor<void> {
 
   @override
   void visitBinaryExpression(BinaryExpression node) {
-    super.visitBinaryExpression(node);
-
-    if (!node.operator.type.isComparisonOperator) return;
-    if ((node.leftOperand.isConstant) && (!node.rightOperand.isConstant)) {
+    if (node case BinaryExpression(
+      leftOperand: final leftOperand,
+      rightOperand: final rightOperand,
+      operator: Token(
+        type: TokenType.EQ_EQ || TokenType.BANG_EQ, // Example, replace with the actual operator type you want to match
+      ),
+    ) when (leftOperand.isConstant) && (!rightOperand.isConstant)) {
       rule.reportAtNode(node);
     }
+
+    super.visitBinaryExpression(node);
   }
 }
