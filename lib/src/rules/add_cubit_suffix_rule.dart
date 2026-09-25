7a10628177c33c09eb88df5c6cf0dc728dfa4942
Laurent Couturier <couturier.laurent75@gmail.com>
2026-09-25 18:03:33 +0200

    refactor(add_cubit_suffix_rule): update visitor to use pattern matching for class declaration
    * Replaced the cubit class visitor with a more general class declaration visitor.
    * Utilized pattern matching to simplify the logic for checking class names.
    * Improved code readability and maintainability.

--- lib/src/rules/add_cubit_suffix_rule.dart
+++ lib/src/rules/add_cubit_suffix_rule.dart
@@ -2,8 +2,9 @@ import 'package:analyzer/analysis_rule/analysis_rule.dart';
 import 'package:analyzer/analysis_rule/rule_context.dart';
 import 'package:analyzer/analysis_rule/rule_visitor_registry.dart';
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/error/error.dart';
-import 'package:my_lints/src/common/rule_visitor_extensions.dart';
+import 'package:my_lints/src/common/extensions.dart';
 
 class AddCubitSuffixRule extends AnalysisRule {
   static LintCode code = const LintCode('add_cubit_suffix_rule', 'Consider add cubit suffix to {0}.');
@@ -16,20 +17,19 @@ class AddCubitSuffixRule extends AnalysisRule {
   @override
   void registerNodeProcessors(RuleVisitorRegistry registry, RuleContext context) {
     final visitor = _Visitor(this);
-    registry.addCubitClass(this, visitor);
+    registry.addClassDeclaration(this, visitor);
   }
 }
 
-class _Visitor extends CustomAstVisitor {
+class _Visitor extends SimpleAstVisitor<void> {
   final AddCubitSuffixRule rule;
 
   _Visitor(this.rule);
 
   @override
-  void visitCubitClass(ClassDeclaration node) {
-    final name = node.name.lexeme;
-    if (name.endsWith('Cubit')) return;
-
-    rule.reportAtToken(node.name, arguments: [name]);
+  void visitClassDeclaration(ClassDeclaration node) {
+    if (node case ClassDeclaration(isCubitClass: true, :final name) when !name.lexeme.endsWith('Cubit')) {
+      rule.reportAtToken(name, arguments: [name.lexeme]);
+    }
   }
 }
